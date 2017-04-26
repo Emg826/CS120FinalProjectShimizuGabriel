c24bf57573e7a686dd055dd84a12bce1ae28fa3c
Akatoki <sshimizu19@wooster.edu>
2017-04-26 15:17:34 -0400

    Update Time.h

--- Time.h
+++ Time.h
@@ -2,35 +2,36 @@
 #define TIME_H
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class Time{
+class Time {
 public:
-  // Default Time Constructor
-  // Pre: none
-  // Post: creates Time object with default values
-  Time();
-  
-  // Parametrized Time Constructor
-  // Pre: 0 <= hour <= 23 and 0 <= min <= 59
-  // Post: creates Time object with given values
-  Time(int hr, int min);
-  
-  // Elapse Time
-  // Pre: none
-  // Post: returns difference in minutes between t1 and t2
-  //       0 minutes <= |t2-t1| <= 1439 minutes
-  static int elapseMin(Time t1, Time t2);
-  
-  // Time in string form
-  // Pre: none
-  // Post: returns time as string as military time
-  string toString();
-  
+	// Default Time Constructor
+	// Pre: none
+	// Post: creates Time object with default values
+	Time();
+
+	// Parametrized Time Constructor
+	// Pre: 0 <= hour <= 23 and 0 <= min <= 59
+	// Post: creates Time object with given values
+	Time(int hr, int min) throw (logic_error);
+
+	// Elapse Time
+	// Pre: none
+	// Post: returns difference in minutes between t1 and t2
+	//       0 minutes <= |t2-t1| <= 1439 minutes
+	static int elapseMin(Time t1, Time t2);
+
+	// Time in string form
+	// Pre: none
+	// Post: returns time as string as military time
+	string toString();
+
 private:
-  int hour;
-  int minute;
-  
+	int hour;
+	int minute;
+
 }
 #endif
