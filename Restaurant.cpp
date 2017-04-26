1389f5a23aff2d519234a259e16778a36ff90c2d
Eric Gabriel <eric.mgabriel@yahoo.com>
2017-04-26 16:13:50 -0400

    Update Restaurant.cpp

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -3,12 +3,27 @@
 // Default Constructor
 Restaurant::Restaurant() {};
 
+
 // Status of orders and drivers
 // Pre: none
 // Post: prints status of orders and logged-in drivers
 void Restaurant::status() {
-
+	// Cooking order status
+    	cout << "Orders in Cooking Queue Status: " << ordersInCooking << endl;
+	
+        // Departure order status
+        cout << "Orders in Departure Queue Status: " << ordersInDeparture << endl;
+        
+        // Driver and their orders status 
+	cout << "Driver Status: " << end;
+	int loginStatus, 
+	for(int i = 0; i < driverVector.size(); i++){
+		   cout << driverVector[i].getName()
+			<< driverVecto
+			<< :
+	}
 }
+
 
 // Summary statistics
 // Pre: none
@@ -18,8 +33,9 @@ void Restaurant::summary() {
 
 }
 
-// Return Driver name
-Driver Restaurant::getDriver(string driverName) {    
+
+// Return Driver object
+Driver& Restaurant::getDriver(string driverName) {    
 	for(int i = 0; i < driverVector.size(); i++) {
         	if(driverVector[i].getName() == driverName)
 			return &driverVector[i];
@@ -29,15 +45,19 @@ Driver Restaurant::getDriver(string driverName) {
     return nullptr;
 }
 
+
 // Employ a driver
 void Restaurant::addDriver(Driver driver) {
 	driverVector.push_back(driver);
 }
 
+
 // Add order
 void Restaurant::addOrder(Order order) {
 	cookingQueue.push(order);
+	ordersInCookingQueue++;
 }
+
 
 // Serve next order
 // Pre: cooking queue not empty
@@ -47,9 +67,14 @@ void Restaurant::serveNextOrder() throw(logic_error) {
 		throw logic_error("Error: cooking queue empty; cannot serve another order");
 
 	Order readyForDelivery = cookingQueue.front();  // Copy order to move it to delivery
+	
 	cookingQueue.pop();                             // Remove readyForDelivery from cookingQueue
+	ordersInCookingQueue--;
+
 	departureQueue.push(readyForDelivery);           // Push readyForDeliver to departureQueue
+	ordersInDeparture++;
 }
+
 
 // Depart next order
 // Pre: none
@@ -59,18 +84,22 @@ Order Restaurant::departNextOrder() throw(logic_error) {
 		throw logic_error("Error: departure queue is empty; cannot deliver an order");
 
 	Order oldestOrder = departureQueue.front();
+	
 	departureQueue.pop();
-
+	ordersInDeparture--;
+	
 	// need way to select driver to deliver
-
 	return oldestOrder;
 }
+
 
 // Deliver order
 // Pre: none
 // Post: order carried by driver delivered @ given time; driver gets tip
 void Restaurant::deliver(Driver driver, Time time, float tip) {
-	Driver driverInVector = getDriver(driver.getName());
+	Driver driverWhoDelivered = getDriver(driver.getName());
+	
+	
 
 
 }
