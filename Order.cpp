9af9fba16c24515d1403ef0b603b72978f3c0911
Akatoki <sshimizu19@wooster.edu>
2017-04-26 14:13:03 -0400

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -1,1 +1,49 @@
-//Order.cpp
+#include <string>
+#include "Order.h"
+
+Order::Order()
+{
+	stateOfOrder = 0;
+}
+
+Order::Order(Time time, string info)
+{
+	stateOfOrder = 0;
+	orderedTime = time;
+	orderInfo = info;
+}
+
+void Order::depart() throw(logic_error)
+{
+	if (stateOfOrder == 0)
+		stateOfOrder++;
+	else
+		throw logic_error("Order is not at the restaurant.");
+}
+
+void Order::deliver(Time time) throw(logic_error)
+{
+	if (stateOfOrder == 1)
+	{
+		stateOfOrder++;
+		deliveredTime = time;
+	}
+	else
+		throw logic_error("Order is not out for delivery.");
+}
+
+int Order::getMinToDelivery() throw(logic_error)
+{
+	if (stateOfOrder == 2)
+	{
+		return Time::elapseMin(deliveredTime, orderedTime);
+	}
+	else
+		throw logic_error("Order is not delivered.");
+}
+
+string Order::toString()
+{
+	string timeAndInfo = orderedTime.toString() + orderInfo;
+	return timeAndInfo;
+}
