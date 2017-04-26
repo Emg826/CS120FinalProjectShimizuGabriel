99031a600e82f433a2f9665039b91a2c934b8598
Eric Gabriel <eric.mgabriel@yahoo.com>
2017-04-26 13:24:32 -0400

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,1 +1,31 @@
-//Time.cpp
+//  Time.cpp
+//  IMPLEMENTATION OF TIME CLASS
+#include "Time.h"
+using namespace std;
+
+// Default Time Constructor
+Time::Time(){
+  hour = 0;
+  minute = 0;
+}
+  
+// Parametrized Time Constructor
+Time::Time(int hr, int min){
+  hour = hr;
+  minute = min;
+}
+  
+// Elapse Time
+static int Time::elapseMin(Time t1, Time t2){
+  static int t1TotalMin = t1.minute + (t1.hour * 60);
+  static int t2TotalMin = t2.minute + (t2.hour * 60);
+  
+  return t2TotalMin - t1TotalMin;
+}
+  
+// Time in string form
+string Time::toString(){
+  string currentTime = hour + ":" + t1.minute
+      
+  return currentTime;
+}
