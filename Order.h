4b6a4f9d333398fdcfb378abefb0d6534009885e
Akatoki <sshimizu19@wooster.edu>
2017-04-26 13:20:56 -0400

    Update Order.h

--- Order.h
+++ Order.h
@@ -1,1 +1,24 @@
-// order class heasder
+// order class header
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <iostream>
+
+
+using namespace std;
+
+class Order
+{
+    public:
+        Order();
+        Order(Time time, string info);
+        void depart() throw (logic_error);
+        void deliver(Time time) throw (logic_error);
+        int getMinToDelivery() throw (logic_error);
+        string toString();
+
+    private:
+
+};
+
+#endif
