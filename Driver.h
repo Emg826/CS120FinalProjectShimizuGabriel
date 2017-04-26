7dbcbdf464e6938be6ae60db354a84f4e42f6896
Akatoki <sshimizu19@wooster.edu>
2017-04-26 13:20:14 -0400

    Update Driver.h

--- Driver.h
+++ Driver.h
@@ -1,1 +1,33 @@
+//Driver.h
+#ifndef DRIVER_H
+#define DRIVER_H
 
+#include <iostream>
+
+
+using namespace std;
+
+class Driver
+{
+    public:
+        Driver(string name);
+        void login() throw (logic_error);
+        void logout() throw (logic_error);
+        void depart(Time time, Order o) throw (logic_error);
+        void deliver(Time time, float tip) throw (logic_error);
+        void arrive(Time time) throw (logic_error);
+        string getName();
+        bool isLoggedIn();
+        int getTotalDeliveries();
+        int getTotalMinDelivering();
+        int getTotalMinDriving();
+        float getTotalTips();
+        Order getOrder() throw (logic_error);
+        string toString();
+
+
+    private:
+
+};
+
+#endif
