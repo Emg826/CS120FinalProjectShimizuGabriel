073fc0f388a38a29a9bdc6318217b83b64180d26
Akatoki <sshimizu19@wooster.edu>
2017-04-26 15:16:39 -0400

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -1,31 +1,38 @@
 //  Time.cpp
 //  IMPLEMENTATION OF TIME CLASS
 #include "Time.h"
+
 using namespace std;
 
 // Default Time Constructor
-Time::Time(){
-  hour = 0;
-  minute = 0;
+Time::Time() {
+	hour = 0;
+	minute = 0;
 }
-  
+
 // Parametrized Time Constructor
-Time::Time(int hr, int min){
-  hour = hr;
-  minute = min;
+Time::Time(int hr, int min) throw (logic_error) {
+	if (0 <= hr && hr <= 23 && 0 <= min && min <= 59)
+	{
+		hour = hr;
+		minute = min;
+	}
+	else
+		throw logic_error("Invalid hour(0-23) and minute(0-59).");
+	
 }
-  
+
 // Elapse Time
-static int Time::elapseMin(Time t1, Time t2){
-  static int t1TotalMin = t1.minute + (t1.hour * 60);
-  static int t2TotalMin = t2.minute + (t2.hour * 60);
-  
-  return t2TotalMin - t1TotalMin;
+static int Time::elapseMin(Time t1, Time t2) {
+	static int t1TotalMin = t1.minute + (t1.hour * 60);
+	static int t2TotalMin = t2.minute + (t2.hour * 60);
+
+	return t2TotalMin - t1TotalMin;
 }
-  
+
 // Time in string form
-string Time::toString(){
-  string currentTime = hour + ":" + t1.minute
-      
-  return currentTime;
+string Time::toString() {
+	string currentTime = hour + ":" + minute;
+
+	return currentTime;
 }
