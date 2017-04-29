d8967a26a96c7a050ecbe86cb3a2f083a26e704c
Eric Gabriel <eric.mgabriel@yahoo.com>
2017-04-28 22:37:20 -0400

    Update Restaurant.cpp
    Restaurant.cpp
    new status function 
    fixed getDriver function
    fixed addOrder
    changed Driver  to Driver* for first parameter type of deliver
    eliminated deliver function; it is redundant in this class, can just 	use the one in Driver class
    added selectDriverToDeliver to randomly select available driver
    made driverVector of pointers so as to access original driver objects
    made addDriver parameter a pass by address

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -5,83 +5,105 @@ Restaurant::Restaurant() {};
 
 
 // Status of orders and drivers
-// Pre: none
-// Post: prints status of orders and logged-in drivers
 void Restaurant::status() {
 	// Cooking order status
-    	cout << "Orders in Cooking Queue Status: " << ordersInCooking << endl;
+    cout << "STATUS OF COOKING QUEUE" << endl;
+    cout << "Orders in Cooking Queue: " << ordersInCooking << endl << endl; // # of orders in cooking
 	
-        // Departure order status
-        cout << "Orders in Departure Queue Status: " << ordersInDeparture << endl;
+    // Departure order status
+    cout << "STATUS OF DEPARTURE QUEUE" << endl;
+    cout << "Orders in Departure Queue: " << ordersInDeparture << endl << endl; // # orders in departure
         
-        // Driver and their orders status 
-	cout << "Driver Status: " << end;
-	int loginStatus, 
-	for(int i = 0; i < driverVector.size(); i++){
-		   cout << driverVector[i].getName()
-			<< driverVecto
-			<< :
+    // Drivers and their orders status
+	cout << "STATUS OF DELIVERY DRIVERS" << endl;
+	for(int i = 0; i < driverVector.size(); i++) {
+        cout << "DRIVER " << i+1 << ": ";                    // driver number
+        cout << driverVector[i]->toString() << endl;        // name and what driver is doing
+		
+        if(driverVector[i]->getDriverState() == 2)          // if driver currently delivering...
+            cout << "...order details: " << driverVector[i]->getOrder().toString() << endl; // time & info
+        
+        cout << "Tips: " << driverVector[i]->getTotalTips() << endl;   // tips
+        cout << "Minutes Delivering: " << driverVector[i]->getTotalMinDriving() << endl; // drive min
+        cout << "Deliveries: " << driverVector[i]->getTotalDeliveries() << endl; // deliveries
+        
+        cout << endl;
 	}
+    
+    cout << endl << endl;
 }
 
 
 // Summary statistics
-// Pre: none
-// Post: print summary statistics on orders and drivers
+// * will add more statistics later on...just want to see if this all works first
 void Restaurant::summary() {
-	// print 
-
+    float totalTips = 0, totalDriveMins = 0, totalDeliveries = 0;
+    
+    for(int i = 0; i < driverVector.size(); i++) {
+        totalTips += driverVector[i]->getTotalTips();
+        totalDriveMins += driverVector[i]->getTotalMinDriving();
+        totalDeliveries += driverVector[i]->getTotalDeliveries();
+    }
+    
+    cout << "\n\n\n\n\n";
+    cout << "RESTAURANT SUMMARY" << endl << endl;
+    
+    // Totals
+    cout << "Total tips: \t" << totalTips << endl;
+    cout << "Total minutes driving: \t" << totalDriveMins << endl;
+    cout << "Total orders delivered: \t"    << totalDeliveries << endl << endl;
+    
+    // Averages
+    cout << "Average tip per driver: \t$" << totalTips / driverVector.size() << endl;
+    cout << "Average minutes driving per driver: \t" << totalDriveMins / driverVector.size() << endl;
+    cout << "Average deliveries per driver: \t" << totalDeliveries / driverVector.size() << endl << endl;
 }
 
 
 // Return Driver object
-Driver& Restaurant::getDriver(string driverName) {    
+Driver* Restaurant::getDriver(string driverName) {
 	for(int i = 0; i < driverVector.size(); i++) {
-        	if(driverVector[i].getName() == driverName)
-			return &driverVector[i];
+        	if(driverVector[i]->getName() == driverName)
+                return driverVector[i];
     }
     
-    // If haven't already returned, then driver does not exist
-    return nullptr;
+    return nullptr; // if haven't already returned, then driver does not exist
 }
 
 
 // Employ a driver
-void Restaurant::addDriver(Driver driver) {
-	driverVector.push_back(driver);
+void Restaurant::addDriver(Driver& driver) {
+	driverVector.push_back(&driver);
 }
 
 
 // Add order
 void Restaurant::addOrder(Order order) {
 	cookingQueue.push(order);
-	ordersInCookingQueue++;
+	ordersInCooking++;
 }
 
 
 // Serve next order
-// Pre: cooking queue not empty
-// Post: removes oldest order from cooking queue; enqueues for delivery
 void Restaurant::serveNextOrder() throw(logic_error) {
 	if (cookingQueue.empty())
-		throw logic_error("Error: cooking queue empty; cannot serve another order");
+		throw logic_error("Error: cooking queue empty; cannot serve another order\n");
 
-	Order readyForDelivery = cookingQueue.front();  // Copy order to move it to delivery
+	Order readyForDelivery = cookingQueue.front();  // copy order to move it to delivery
 	
-	cookingQueue.pop();                             // Remove readyForDelivery from cookingQueue
-	ordersInCookingQueue--;
+	cookingQueue.pop();                             // remove readyForDelivery from cookingQueue
+	ordersInCooking--;
 
-	departureQueue.push(readyForDelivery);           // Push readyForDeliver to departureQueue
+	departureQueue.push(readyForDelivery);          // push readyForDeliver to departureQueue
 	ordersInDeparture++;
 }
 
 
 // Depart next order
-// Pre: none
-// Post: removes oldest order from departure queue and returns it
+// * would do something like:  driverXYZ.depart(currentTime, restaurantZYX.departNextOrder())
 Order Restaurant::departNextOrder() throw(logic_error) {
 	if (departureQueue.empty())
-		throw logic_error("Error: departure queue is empty; cannot deliver an order");
+		throw logic_error("Error: departure queue is empty; cannot deliver an order\n");
 
 	Order oldestOrder = departureQueue.front();
 	
@@ -92,14 +114,24 @@ Order Restaurant::departNextOrder() throw(logic_error) {
 	return oldestOrder;
 }
 
-
-// Deliver order
-// Pre: none
-// Post: order carried by driver delivered @ given time; driver gets tip
-void Restaurant::deliver(Driver driver, Time time, float tip) {
-	Driver driverWhoDelivered = getDriver(driver.getName());
-	
-	
-
-
+/* DOES NOT WORK CORRECTLY AS OF NOW
+// Select random available driver to deliver oldest order in departureQueue
+Driver* Restaurant::selectDriverToDeliver() throw(logic_error){
+    vector<Driver> availableDrivers;
+    
+    for(int i = 0; i < driverVector.size(); i++) {  // compile list of available drivers
+        if(driverVector[i].getDriverState() == 1)
+            availableDrivers.push_back(driverVector[i]);
+    }
+    
+    if(!availableDrivers.empty()){    // if there are available drivers
+        srand(int(time(NULL)));
+        int randIndex = rand() % availableDrivers.size();
+        return getDriver(availableDrivers[randIndex].getName());    // Return random but available driver
+    }
+    
+    
+    cout << "No available drivers\n" << endl;
+    return nullptr; // if haven't already returned, then driver no available drivers
 }
+*/
